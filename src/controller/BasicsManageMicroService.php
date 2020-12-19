407be42d93103ee5c2f3f733766fe089afe5115a
pi <pizepei@pizepei.com>
2020-12-19 23:29:30 +0800

    迁移微服务中心

--- src/controller/BasicsManageMicroService.php
+++ src/controller/BasicsManageMicroService.php
@@ -2,14 +2,9 @@
 /**
  * Created by PhpStorm.
  * User: pizepei
- * Date: 2019/10/21
+ * Date: /10/21
  * Time: 11:28
- * @baseControl normphpCore\microServices\src\controller\BasicsMicroservice
- * @baseAuth
  * @title 应用端微服务管理
- * @authGroup [user:用户相关,admin:管理员相关] 权限组列表
- * @basePath /basics/microservice/
- * @baseParam [$Request:normphp\staging\Request] 注册依赖注入对象
  */
 
 
--- src/controller/BasicsMicroServiceApi.php
+++ src/controller/BasicsMicroServiceApi.php
@@ -4,12 +4,7 @@
  * User: pizepei
  * Date: 2019/10/21
  * Time: 11:28
- * @baseControl normphpCore\microServices\src\controller\BasicsMicroservice
- * @baseAuth
  * @title 应用端微服务管理
- * @authGroup [user:用户相关,admin:管理员相关] 权限组列表
- * @basePath /basics/microservice/
- * @baseParam [$Request:normphp\staging\Request] 注册依赖注入对象
  */
 
 
